ebe2994d866f7f0a23e856c10c69c11dff438e95
sonnett09 <waveenhermosura09@gmail.com>
2026-04-23 01:46:03 +0800

    wc debug

--- javascript/manga/src/en/weebcentral.js
+++ javascript/manga/src/en/weebcentral.js
@@ -16,55 +16,65 @@ class DefaultExtension extends MProvider {
         super();
         this.client = new Client();
     }
+
     getHeaders(url) {
         return { "Referer": `${this.source.baseUrl}/` };
     }
 
     async request(slug) {
-        var url = `${this.source.baseUrl}${slug}`
+        var url = `${this.source.baseUrl}${slug}`;
         var res = await this.client.get(url);
         return new Document(res.body);
     }
 
-
     async getPopular(page) {
         const filters = this.getFilterList();
         filters[0].state = 2;
-        return await this.search("", page, filters)
+        return await this.search("", page, filters);
     }
 
     async getLatestUpdates(page) {
         const filters = this.getFilterList();
         filters[0].state = 5;
-        return await this.search("", page, filters)
+        return await this.search("", page, filters);
     }
 
-    getImageUrl(id) { return `https://temp.compsci88.com/cover/normal/${id}.webp`; }
+    getImageUrl(id) {
+        return `https://temp.compsci88.com/cover/normal/${id}.webp`;
+    }
 
     async search(query, page, filters) {
-        var offset = 32 * (parseInt(page) - 1)
-        var sort = filters[0].values[filters[0].state].value
-        var order = filters[1].values[filters[1].state].value
-        var translation = filters[2].values[filters[2].state].value
-        var status = ""
+        if (!filters || !filters[0] || !filters[0].values) {
+            filters = this.getFilterList();
+        }
+
+        var offset = 32 * (parseInt(page) - 1);
+        var sort = (filters[0].values[filters[0].state] ?? filters[0].values[0]).value;
+        var order = (filters[1].values[filters[1].state] ?? filters[1].values[0]).value;
+        var translation = (filters[2].values[filters[2].state] ?? filters[2].values[0]).value;
+
+        var status = "";
         for (var filter of filters[3].state) {
             if (filter.state == true)
-                status += `&included_status=${filter.value}`
+                status += `&included_status=${filter.value}`;
         }
-        var type = ""
+
+        var type = "";
         for (var filter of filters[4].state) {
             if (filter.state == true)
-                type += `&included_type=${filter.value}`
+                type += `&included_type=${filter.value}`;
         }
-        var tags = ""
+
+        var tags = "";
         for (var filter of filters[5].state) {
             if (filter.state == true)
-                tags += `&included_tag=${filter.value}`
+                tags += `&included_tag=${filter.value}`;
         }
-        var slug = `/search/data?limit=32&offset=${offset}&author=&text=${query}&sort=${sort}&order=${order}&official=${translation}${status}${type}${tags}&display_mode=Full%20Display`
+
+        var slug = `/search/data?limit=32&offset=${offset}&author=&text=${query}&sort=${sort}&order=${order}&official=${translation}${status}${type}${tags}&display_mode=Full%20Display`;
         var doc = await this.request(slug);
         var list = [];
-        var mangaElements = doc.select("article:has(section)")
+        var mangaElements = doc.select("article:has(section)");
         for (var manga of mangaElements) {
             var imageUrl = manga.selectFirst("img").getSrc;
             var details = manga.selectFirst("section > a");
@@ -74,9 +84,9 @@ class DefaultExtension extends MProvider {
         }
 
         var hasNextPage = doc.selectFirst("button").text.length > 0;
-        return { list, hasNextPage }
-
+        return { list, hasNextPage };
     }
+
     statusCode(status) {
         return {
             "Ongoing": 0,
@@ -92,48 +102,51 @@ class DefaultExtension extends MProvider {
         var slug = url.startsWith("http") ? `/series/${link}` : `/series/${url}`;
         var doc = await this.request(slug);
         var imageUrl = url.startsWith("http") ? this.getImageUrl(link) : this.getImageUrl(url);
-        var description = doc.selectFirst("p.whitespace-pre-wrap.break-words").text
+        var description = doc.selectFirst("p.whitespace-pre-wrap.break-words").text;
 
-        var chapters = []
-        var ul = doc.select("ul.flex.flex-col.gap-4 > li")
-        var author = ""
-        var genre = []
-        var status = 5
+        var chapters = [];
+        var ul = doc.select("ul.flex.flex-col.gap-4 > li");
+        var author = "";
+        var genre = [];
+        var status = 5;
         for (var li of ul) {
-            var strongTxt = li.selectFirst("strong").text
+            var strongTxt = li.selectFirst("strong").text;
             if (strongTxt.indexOf("Author(s):") != -1) {
-                author = li.selectFirst("a").text
+                author = li.selectFirst("a").text;
             } else if (strongTxt.indexOf("Tags(s):") != -1) {
-                li.select("a").forEach(a => genre.push(a.text))
+                li.select("a").forEach(a => genre.push(a.text));
             } else if (strongTxt.indexOf("Status:") != -1) {
-                status = this.statusCode(li.selectFirst("a").text)
+                status = this.statusCode(li.selectFirst("a").text);
             }
-
         }
 
-        var chapSlug = `${slug}/full-chapter-list`
+        var chapSlug = `${slug}/full-chapter-list`;
         doc = await this.request(chapSlug);
         var chapList = doc.select("div.flex.items-center");
         for (var chap of chapList) {
-            var name = chap.selectFirst("span.grow.flex.items-center.gap-2").selectFirst("span").text
-            var dateUpload = new Date(chap.selectFirst("time.text-datetime").text).valueOf().toString()
-            var url = chap.selectFirst("input").attr("value")
-            chapters.push({ name, url, dateUpload })
+            var name = chap.selectFirst("span.grow.flex.items-center.gap-2").selectFirst("span").text;
+            var dateUpload = new Date(chap.selectFirst("time.text-datetime").text).valueOf().toString();
+            var url = chap.selectFirst("input").attr("value");
+            chapters.push({ name, url, dateUpload });
         }
-        return { description, imageUrl, author, genre, status, chapters }
 
+        return { description, imageUrl, author, genre, status, chapters };
     }
+
     async getPageList(url) {
-        var slug = `/chapters/${url}/images?current_page=1&reading_style=long_strip`
+        var slug = `/chapters/${url}/images?current_page=1&reading_style=long_strip`;
         var doc = await this.request(slug);
-
         var urls = [];
-
-        doc.select("section > img").forEach(page => urls.push(page.attr("src")))
-
-        return urls.map(x => ({ url: x, headers: { Referer: `${this.source.baseUrl}/`, Accept: "image/avif,image/webp,*/*", Host: `${x.match(/^(?:https?:\/\/)?([^\/:]+)(:\d+)?/)[1]}` } }));
+        doc.select("section > img").forEach(page => urls.push(page.attr("src")));
+        return urls.map(x => ({
+            url: x,
+            headers: {
+                Referer: `${this.source.baseUrl}/`,
+                Accept: "image/avif,image/webp,*/*",
+                Host: `${x.match(/^(?:https?:\/\/)?([^\/:]+)(:\d+)?/)[1]}`
+            }
+        }));
     }
-
 
     getFilterList() {
         return [
@@ -149,7 +162,8 @@ class DefaultExtension extends MProvider {
                     ["Recently Added", "Recently Added"],
                     ["Latest Updates", "Latest Updates"]
                 ].map(x => ({ type_name: 'SelectOption', name: x[0], value: x[1] }))
-            }, {
+            },
+            {
                 type_name: "SelectFilter",
                 name: "Order",
                 state: 0,
@@ -157,7 +171,8 @@ class DefaultExtension extends MProvider {
                     ["Ascending", "Ascending"],
                     ["Descending", "Descending"]
                 ].map(x => ({ type_name: 'SelectOption', name: x[0], value: x[1] }))
-            }, {
+            },
+            {
                 type_name: "SelectFilter",
                 name: "Official Translation",
                 state: 0,
@@ -166,7 +181,8 @@ class DefaultExtension extends MProvider {
                     ["True", "True"],
                     ["False", "False"],
                 ].map(x => ({ type_name: 'SelectOption', name: x[0], value: x[1] }))
-            }, {
+            },
+            {
                 type_name: "GroupFilter",
                 name: "Series Status",
                 state: [
@@ -175,7 +191,8 @@ class DefaultExtension extends MProvider {
                     ["Hiatus", "Hiatus"],
                     ["Canceled", "Canceled"],
                 ].map(x => ({ type_name: 'CheckBox', name: x[0], value: x[1] }))
-            }, {
+            },
+            {
                 type_name: "GroupFilter",
                 name: "Series Type",
                 state: [
@@ -184,7 +201,8 @@ class DefaultExtension extends MProvider {
                     ["Manhua", "Manhua"],
                     ["OEL", "OEL"],
                 ].map(x => ({ type_name: 'CheckBox', name: x[0], value: x[1] }))
-            }, {
+            },
+            {
                 type_name: "GroupFilter",
                 name: "Tags",
                 state: [
@@ -227,6 +245,6 @@ class DefaultExtension extends MProvider {
                     ["Other", "Other"]
                 ].map(x => ({ type_name: 'CheckBox', name: x[0], value: x[1] }))
             }
-        ]
+        ];
     }
 }
